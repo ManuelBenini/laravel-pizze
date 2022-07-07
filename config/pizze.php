97bd17d9c8685396c9decb8e7a175f77c85fe3a8
ManuelBenini <manuelbenini1905@gmail.com>
2022-07-07 10:07:49 +0200

    Primo commit, creata rotta di gruppo (admin), create cartelle admin e guest, creato PageController

---
+++ config/pizze.php
@@ -0,0 +1,49 @@
+<?php 
+
+$pizze = [
+	[
+		"nome" => "Margherita", 
+		"prezzo" => 4.5, 
+		"ingredienti" => "Pomodoro, Mozzarella", 
+		"vegetariana" => "sì" 
+    ], 
+	[
+		"nome" => "Tonno & Cipolla", 
+		"prezzo" => 7.5, 
+		"ingredienti" => "Pomodoro, Mozzarella, Tonno, Cipolla", 
+		"vegetariana" => "no" 
+	], 
+	[
+		"nome" => "Prosciutto & Funghi", 
+		"prezzo" => 5.5, 
+		"ingredienti" => "Pomodoro, Mozzarella, Prosciutto Cotto, Funghi", 
+		"vegetariana" => "no" 
+	], 
+	[
+		"nome" => "Quattro Formaggi", 
+		"prezzo" => 8, 
+		"popolarita" => 6, 
+		"ingredienti" => "Mozzarella, Gorgonzola, Emmental, Asiago", 
+		"vegetariana" => "sì" 
+	], 
+	[
+		"nome" => "Wurstel", 
+		"prezzo" => 5, 
+		"ingredienti" => "Pomodoro, Mozzarella, Wurstel", 
+		"vegetariana" => "no" 
+	], 
+	[
+		"nome" => "Diavola", 
+		"prezzo" => 6, 
+		"ingredienti" => "Pomodoro, Mozzarella, Salamino Piccante", 
+		"vegetariana" => "no" 
+	], 
+	[
+		"nome" => "Capricciosa", 
+		"prezzo" => 7.5, 
+		"ingredienti" => "Pomodoro, Mozzarella, Prosciutto Cotto, Funghi, Carciofi, Olive", 
+		"vegetariana" => "no" 
+	] 
+]; 
+ 
+ 
