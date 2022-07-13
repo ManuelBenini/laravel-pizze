373d363b82efb949523a018ef4f6d79a100667af
ManuelBenini <manuelbenini1905@gmail.com>
2022-07-13 13:44:14 +0200

    Pulizia codice

--- database/migrations/2022_07_12_142624_create_ingredient_pizza_table.php
+++ database/migrations/2022_07_12_142624_create_ingredient_pizza_table.php
@@ -14,21 +14,11 @@ class CreateIngredientPizzaTable extends Migration
     public function up()
     {
         Schema::create('ingredient_pizza', function (Blueprint $table) {
-            // $table->id();
-            // $table->timestamps();
-
             $table->unsignedBigInteger('pizza_id');
             $table->unsignedBigInteger('ingredient_id');
 
-            $table->foreign('pizza_id')
-                                    ->references('id')
-                                    ->on('pizzas')
-                                    ->onDelete('cascade');
-
-            $table->foreign('ingredient_id')
-                                    ->references('id')
-                                    ->on('ingredients')
-                                    ->onDelete('cascade');
+            $table->foreign('pizza_id')->references('id')->on('pizzas')->onDelete('cascade');
+            $table->foreign('ingredient_id')->references('id')->on('ingredients')->onDelete('cascade');
         });
     }
 
